23f2bd5efb486c319edcbdf5a85836050b0568c0
Will Yngson <christianwillyngson@gmail.com>
2021-08-13 11:33:23 +0800

    added meta tag for mobile layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import 'styles/global.scss';
 import type {AppProps} from 'next/app';
 
 function MyApp({Component, pageProps}: AppProps) {
     return (
         <>
+            <Head>
+                <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0" />
+            </Head>
             <Component {...pageProps} />
         </>
     )
